bfd6270a0204cb1bc40837b463a13e2485b780ae
anand2312 <anandkrishna2312@gmail.com>
2022-01-26 21:48:22 +0400

    Add rolename handlers to router

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ use serde_json::json;
 use worker::*;
 
 mod utils;
+mod rolename;
+
 
 fn log_request(req: &Request) {
     console_log!(
@@ -24,6 +26,9 @@ pub async fn main(req: Request, env: Env) -> Result<Response> {
 
     router
         .get_async("/", |_, _| async move { Response::ok("utilitybot miscellaneous backends worker. why are you here?") })
+        .get_async("/rolenames", rolename::get)
+        .post_async("/rolenames", rolename::post)
+        .delete_async("/rolenames", rolename::delete)
         .run(req, env)
         .await
 }
